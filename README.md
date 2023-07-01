8f6421ca681798f49fb34c4b6ec5cd6af8261302
snyster <137181227+snyster@users.noreply.github.com>
2023-07-01 20:43:12 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# TripWorld2
+
+First website created as the final project for an html + css course, now
+featuring additional gallery for the js course.
---
+++ jScripts/JavaScript.js
@@ -0,0 +1,45 @@
+﻿var currentSlide = 1;
+window.addEventListener('load', function () {
+    showSlides();
+});
+
+function plusOne() {
+    if (currentSlide == 6) {
+        currentSlide = 1;
+    }
+    else {
+        currentSlide++
+    }
+    showSlides()
+}
+function minusOne() {
+    if (currentSlide == 1) {
+        currentSlide = 6;
+    }
+    else {
+        currentSlide--
+    }
+    showSlides()
+}
+function clickSlide(slide) {
+    if (currentSlide == slide) {
+        alert("נמצא על התמונה הנוכחית")
+    }
+    else {
+        currentSlide = slide;
+        showSlides()
+    }
+}
+function showSlides() {
+    for (var i = 1; i < 7; i++) {
+        var id = 'picture' + i;
+        var id2 = 'smallPicture' + i;
+        if (i == currentSlide) {
+            document.getElementById(id).style.display = "inline-block";
+            document.getElementById(id2).classList.add("active")
+        } else {
+            document.getElementById(id).style.display = "none";
+            document.getElementById(id2).classList.remove("active");
+        }
+    }
+}
